c0f63b7aca840c7722f961fb580651930aa339c4
Jimi <fashoyinolujimit@gmail.com>
2022-10-14 09:50:28 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 
 int main(0)
 {
-	printf("Size of a char: 1 byte(s)\n Size of an int: 4 byte(s)\n Size of a long int: 4 byte(s)\n Size of a long long int: 8 byte(s)\n Size of a float: 4 byte(s)\n")
+	printf("Size of a char: 1 byte(s)\n Size of an int: 4 byte(s)\n Size of a long int: 4 byte(s)\n Size of a long long int: 8 byte(s)\n Size of a float: 4 byte(s)\n");
 	return (0);
 
 }
