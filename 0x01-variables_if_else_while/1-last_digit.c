c20c766179380476bc30e8b549518143b56702b5
Jimi <fashoyinolujimit@gmail.com>
2022-10-16 14:42:33 -0700

    if-else

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,11 +2,9 @@
 #include <stdio.h>
 #include <time.h>
 
-/* more headers goes there */
-
-
-
-/* betty style doc for function main goes there */
+/**
+ * If-else practice
+ */
 
 int main(void)
 
@@ -17,22 +15,22 @@ int main(void)
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
 		
-		if (n%10>5)
+		if (n % 10 > 5)
 		{
-			printf("%s%d%s%d%s","Last digit of ",n," is ", n%10," and is greater than 5");
+			printf("%s%d%s%d%s\n" , "Last digit of " , n , " is " , n%10 , " and is greater than 5");
 		
 		}
 		else
 		{
-			if (n%10==0)
+			if (n % 10 == 0)
 			{
-				printf("%s%d%s%d%s","Last digit of ",n," is ", n%10," and is 0\n");
+				printf("%s%d%s%d%s" , "Last digit of " , n ," is " , n % 10 , " and is 0\n");
 			}
 			else
 			{
-				if (n%10<6 && n%10!=0)
+				if (n % 10 < 6 && n % 10 != 0)
 				{
-					printf("%s%d%s%d%s","Last digit of ",n," is ",n%10,"and is less than 6 and not 0\n");
+					printf("%s%d%s%d%s" , "Last digit of ", n ," is " , n%10," and is less than 6 and not 0\n");
 				}
 			}
 			
