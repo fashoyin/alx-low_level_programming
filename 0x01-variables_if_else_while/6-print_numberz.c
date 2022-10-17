6ecef7c6ecddb0d10addc362987995d50a1dc47d
Jimi <fashoyinolujimit@gmail.com>
2022-10-17 14:48:48 -0700

    commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - ALx
+ *
+ *Description: putchar 0-10
+ *
+ * Return: Always true when equal to 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		putchar(i)
+	}
+	putchar("\n")
+}
