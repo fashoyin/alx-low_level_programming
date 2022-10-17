9b198f7204f1a9381b1641bec1bbd83bd4792682
Jimi <fashoyinolujimit@gmail.com>
2022-10-17 03:48:46 -0700

    letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - If-else
+ *
+ * Description: using putchar
+ *
+ * Return: Always true when 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i='a',i<=z,i++)
+	{
+		putchar(i)
+	}
+	putchar('\n')
+}
