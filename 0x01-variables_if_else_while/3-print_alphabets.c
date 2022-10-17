0cea7afa4b9e8b82162baf07b371beacb673e111
Jimi <fashoyinolujimit@gmail.com>
2022-10-17 04:36:34 -0700

    commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,19 +7,18 @@
  *
  * return: always ttrue when zero
  */
-
 int main(void)
 {
 	char i;
 	char j;
-
+	
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		for(j = 'A'; j <= 'Z'; j++)
-		{
-			putchar(i);
-			putchar(j);
-		}
+		putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
 	}
 	putchar('\n');
 	return (0); 
