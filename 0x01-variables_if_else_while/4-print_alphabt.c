71134257cb9033638f799ad22098bb195e8e6e41
Jimi <fashoyinolujimit@gmail.com>
2022-10-17 13:26:50 -0700

    commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,9 +11,16 @@ int main(void)
 {
 	char i;
 
-	for (i = 'a'; i <= 'z' && (i != 'q' && i != 'e'); i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
-		putchar(i);
+		if (i == e || i == q)
+		{
+			i++
+		}
+		else
+		{
+			putchar(i);
+		}
 	}
 	putchar('\n');
 	return (0);
