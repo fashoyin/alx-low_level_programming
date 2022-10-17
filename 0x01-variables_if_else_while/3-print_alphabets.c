a8efbad70b35a5c183e6cb58c15298d30cf7cb91
Jimi <fashoyinolujimit@gmail.com>
2022-10-17 04:21:44 -0700

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main
+ *
+ * Descrption
+ *
+ * return
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a', i <= z, i++)
+	{
+		for(j = 'A', j <= 'Z', j++)
+		{
+			putchar(i,j);
+		
+		}
+	}
+	putchar('\n')
+}
