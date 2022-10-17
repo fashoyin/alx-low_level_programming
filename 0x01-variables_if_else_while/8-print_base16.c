08b40723d256f0f830e83e5e50450138944ffc56
Jimi <fashoyinolujimit@gmail.com>
2022-10-17 15:35:10 -0700

    reverse  letters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -main
+ *
+ * Description: Alx
+ *
+ * Return: Always true when 0
+ */
+
+int main(void)
+{
+	for (i = 0; i <= 16; i++)
+		putchar("%x", i);
+	putchar('\n');
+	return (0);
+}
