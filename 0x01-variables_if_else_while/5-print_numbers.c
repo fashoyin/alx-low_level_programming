a38b7bd74798cb744fbc2316db2cc0f9f1b0ada3
Jimi <fashoyinolujimit@gmail.com>
2022-10-17 14:00:52 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Alx
+ *
+ * Description Baase 10 single digits
+ *
+ * Return: Always true when equsls 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0, i <= 9, i++)
+	{
+		printf('%d', i);
+	}
+	printf('\n');
+	return (0);
+}
