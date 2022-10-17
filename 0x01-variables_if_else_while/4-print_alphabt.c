1e44ccb2f5566a90760f734f8bb609c4a49b2d94
Jimi <fashoyinolujimit@gmail.com>
2022-10-17 13:05:44 -0700

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - The main
+ *
+ * Description: alphabets in lowercase
+ *
+ * Return: Always true when 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z', i++)
+	{
+		if (i == 'q' && i == 'e')
+		{}
+		else
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+}
