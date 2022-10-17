3bd9cc3422411a3905ce1bbc89a51e2d6b7ee568
Jimi <fashoyinolujimit@gmail.com>
2022-10-17 15:52:34 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,8 +12,8 @@ int main(void)
 	int i;
 	char j;
 
-	for (i = 0; i <= 10; i++)
-		putchar((i % 10) + '0');
+	for (i = 0; i <= 9; i++)
+		putchar((i % 9) + '0');
 	for (j = 'a'; j <= 'f'; j++)
 		putchar(j);
 	putchar('\n');
