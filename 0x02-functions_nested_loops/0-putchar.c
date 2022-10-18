a0d5d4421c5f297d81a0bc08fc17c21436f19590
Jimi <fashoyinolujimit@gmail.com>
2022-10-18 15:43:01 -0700

    commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main
+ *
+ * Description: printing _putchar
+ *
+ * Return: returns 0 when true
+ */
+
+int main(void)
+{
+	printf("%s","putchar");
+	return (0);
+}
