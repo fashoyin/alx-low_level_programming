7505c60e7131a843e52776371f40882501b0d7e7
Jimi <fashoyinolujimit@gmail.com>
2022-10-17 18:55:49 -0700

    commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - main
+ *
+ * Description: my description
+ *
+ * return: Always true when 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		for (j = 0; j <=9; j++)
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i = 9 && j = 9)
+			{
+				continue;
+			}
+			putchar(',')
+			putchar(' ')
+	return (0);
+}
