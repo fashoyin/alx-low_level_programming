ea638bf612df83a79d985de651147b021004f722
Jimi <fashoyinolujimit@gmail.com>
2022-10-21 12:54:54 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - searching for character c
+ *
+ * @c: c is the charater 
+ *
+ * Return: 0 if Upper and 1 if lower
+ */
+ 
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90 )
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
