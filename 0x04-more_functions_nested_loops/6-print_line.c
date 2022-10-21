e022f628854a2e0b5eecc84d315f12c02ca2d047
Jimi <fashoyinolujimit@gmail.com>
2022-10-21 16:23:13 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *more_numbers - Print 10 times the numbers since 0 up to 14
+ *
+ *Return: 10 times of the numbers since 0 up to 14
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(95)
+		}
+		_putchar('\n')
+	}
+	else
+		_putchar('\n')
+}
