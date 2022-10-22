509ed6f5c047d7019605355ad534eb47afa1f88b
Jimi <fashoyinolujimit@gmail.com>
2022-10-22 14:45:45 -0700

    debug 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *main - tests function that prints if integer is positive or negative
  *
@@ -11,5 +12,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
