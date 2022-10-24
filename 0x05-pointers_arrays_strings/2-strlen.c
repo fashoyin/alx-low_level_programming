c23ad202b7952f82900944a4fc5abe65c8eafa1a
Jimi <fashoyinolujimit@gmail.com>
2022-10-24 14:51:00 -0700

    pointers 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - Gets the length of a string
+ * @s: The string being considered
+ * Return: Return length of string
+ */
+
+int _strlen(char *s)
+{
+	int a = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		a++;
+	}
+	return (0);
+}
