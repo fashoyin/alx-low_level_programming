f58116da9545009e7ed2ea52b54758a006906a93
Jimi <fashoyinolujimit@gmail.com>
2022-10-24 14:13:04 -0700

    pointers 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * Updating the value in a pointer address
- * @n is the value being updated
+ *reset_to_98 - Updating the value in a pointer address
+ * @n: is the value being updated
  * Return: 0 always succesful
  */
 
