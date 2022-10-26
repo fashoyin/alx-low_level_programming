1a0208f0959cec16d072c2e694464674a27dc233
Jimi <fashoyinolujimit@gmail.com>
2022-10-26 14:31:36 -0700

    commit 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,9 @@
 #include "main.h"
 /**
- * _strcpy - Copy a string
- *
- * @dest: Destination 
- *
- * @src: Source
- * Return: the pointer to dest
+ *_strcpy - Copy a string
+ *@dest: Destination value
+ *@src: Source value
+ *Return: the pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
