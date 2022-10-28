b5a38005e12d69cf714868069a76e24ab00ef980
Jimi <fashoyinolujimit@gmail.com>
2022-10-28 14:26:49 -0700

    commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * reverse_array - Reverse content
+ * @a: array of integers
+ * @n: Number of elements to swap
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int *x, i, j, k;
+
+	x = a;
+
+	for (i = 1; i < n; i++)
+	{
+		p++;
+	}
+	for (k = 0; k < i / 2; k++)
+	{
+		j = a[k];
+		a[k] = *x;
+		*x = j;
+		p--;
+	}
+}
