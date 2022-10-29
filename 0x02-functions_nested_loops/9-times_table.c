668a24a1fbaea589ff3da5c2a6ed12c6bdb6ed40
Jimi <fashoyinolujimit@gmail.com>
2022-10-29 15:30:01 -0700

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,14 +9,31 @@ void times_table(void)
 
 	int i, n;
 
-	int j;
+	int j, u, d;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			n =i * j;
-			_putchar(n + '0');
+			if (n > 9)
+			{
+				u = n % 10;
+				d = (n - u) / 10;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				_putchar(n + '0');
+			}
 		}
 		_putchar('\n');
 	}
