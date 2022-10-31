13dfe9bd1dea8723fe9760380b0136bf1d53f4f0
Jimi <fashoyinolujimit@gmail.com>
2022-10-30 19:12:57 -0700

    numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 	{
 		for (; n < 98; n++)
 		{
-			_putchar(n + '0');
+			printf('%d', n;
 			_putchar(',');
 			_putchar(' ');
 		}
@@ -20,14 +20,14 @@ void print_to_98(int n)
 	{
 		for (; n > 98; --n)
 		{
-			_putchar(n + '0');
+			printf('%d', n);
 			_putchar(',');
 			_putchar(' ');
 		}
 	}
 	else
 	{
-		_putchar('\n');
+		printf('%d', n);
 	}
 	_putchar('\n');
 }
