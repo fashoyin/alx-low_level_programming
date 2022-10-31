9efb222865f6c42a453c00831ce2e582ffdd4792
Jimi <fashoyinolujimit@gmail.com>
2022-10-30 18:51:24 -0700

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 /**
  * add -  adds two integers and returns the results
  * @i: first integer
- * @j - second integer
+ * @j: second integer
  * Return: result
  */
 int add(int i, int j)
@@ -10,5 +10,5 @@ int add(int i, int j)
 	int result;
 
 	result = i + j;
-	return result;
+	return (result);
 }
