8db35ea3321595b47e127a8f516260cf889436e5
Jimi <fashoyinolujimit@gmail.com>
2022-10-30 19:22:25 -0700

    numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,6 +15,7 @@ void print_to_98(int n)
 			_putchar(',');
 			_putchar(' ');
 		}
+		_putchar('\n');
 	}
 	else if (n > 98)
 	{
@@ -24,10 +25,10 @@ void print_to_98(int n)
 			_putchar(',');
 			_putchar(' ');
 		}
+		_putchar('\n');
 	}
 	else
 	{
 		printf("%d", n);
 	}
-	_putchar('\n');
 }
