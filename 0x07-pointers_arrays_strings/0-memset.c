696085d032945488035710e028c3fae7def6175f
Jimi <fashoyinolujimit@gmail.com>
2022-11-01 18:25:48 -0700

    pointer to pointer

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,12 @@
 #include "main.h"
-
 /**
  * _memset -fills the first n bytes of the memory area pointed to by s with the constant byte b
  * @s: pointer to memory area 
  * @n: the number of bytes to fill
  * @b: constant byte
- *
  * Return: a pointer to the memory area @s
  */
-
-
-
 char *_memset(char *s, char b, unsigned int n)
-
 {
 	unsigned int i;
 
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * _memcpy - function copies n bytes from memory area src to memory area dest
+ * @dest:
+ * @src:
+ * @n:
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
